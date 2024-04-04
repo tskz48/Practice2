384cb49274ff4d56004180fe51fa4d4473d6a5a3
Rohith Koti <tskz48@Rohiths-MacBook-Pro-2.local>
2024-04-04 20:40:29 +0100

    FibonacciSequence

---
+++ Fibonaccisequence.py
@@ -0,0 +1,21 @@
+
+n=0
+while n==0 :
+        n=input("Please type in a non zero number.")
+        n=int(n)
+x=0
+i=1
+y=1
+while i <=n:
+
+        print(str(i)+":"+str(x))
+        z=x
+        x=x+ y
+        y=z
+
+
+        i=i+1
+    
+
+  
+
