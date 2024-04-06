41496e4a16ce12821e3d2b5920873cf868a6d01b
Rohith Koti <tskz48@Rohiths-MacBook-Pro-2.local>
2024-04-06 15:51:02 +0100

    SwapNumbers

---
+++ SwapNumbers.py
@@ -0,0 +1,24 @@
+x=input("Type in number 1.")
+y=input("Type in number 2.")
+print("x:"+str(x))
+print("y:"+str(y))
+z=x
+x=y
+y=z
+print("x:"+str(x))
+print("y:"+str(y))
+
+
+x=input("Type in a number.")
+y=input("Type in another number.")
+l=[]
+print("x:"+str(x))
+print("y:"+str(y))
+l.append(x)
+l.append(y)
+x=l[1]
+y=l[0]
+print("x:"+str(x))
+print("y:"+str(y))
+l
+
